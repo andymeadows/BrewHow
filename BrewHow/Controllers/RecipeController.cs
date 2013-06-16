9186285a878a5ab5e90b5e808d26ca4cbaa53028
Andy Meadows <andy.meadows@gmail.com>
2013-06-15 21:29:03 -0400

    Chapter 4 post-migration source
    No pagination yet.  More to come.

--- BrewHow/Controllers/RecipeController.cs
+++ BrewHow/Controllers/RecipeController.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 using BrewHow.Models;
@@ -10,19 +9,17 @@ namespace BrewHow.Controllers
 {
     public class RecipeController : Controller
     {
-        //
-        // GET: /Recipe/
-
         public ActionResult Index()
         {
-            var recipeListModel = new List<Recipe>
-            {
-                new Recipe { Name = "Sweaty Brown Ale", Style = "Brown Ale", OriginalGravity = 1.05f, FinalGravity = 1.01f },
-                new Recipe { Name = "Festive Milk Stout", Style = "Sweet/Milk Stout", OriginalGravity = 1.058f, FinalGravity = 1.015f },
-                new Recipe { Name = "Andy's Heffy", Style = "Heffeweisen", OriginalGravity = 1.045f, FinalGravity = 1.012f }
-            };
+            List<Recipe> recipes = null;
 
-            return View(recipeListModel);
+            using (var context = new BrewHowContext())
+            {
+                recipes = (from recipe in context.Recipes
+                           select recipe).ToList();
+            }
+
+            return View(recipes);
         }
 
     }
--- BrewHow/Models/Recipe.cs
+++ BrewHow/Models/Recipe.cs
@@ -7,9 +7,14 @@ namespace BrewHow.Models
 {
     public class Recipe
     {
+        public int RecipeId { get; set; }
         public string Name { get; set; }
-        public string Style { get; set; }
+        public Style Style { get; set; }
         public float OriginalGravity { get; set; }
         public float FinalGravity { get; set; }
+        public string GrainBill { get; set; }
+        public string Instructions { get; set; }
+
+        public virtual ICollection<Review> Reviews { get; set; }
     }
 }
